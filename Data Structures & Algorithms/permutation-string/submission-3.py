57c21890bcb579093a800c6a60f24f08b3908e68
eggsdie <105503917+eggsdie@users.noreply.github.com>
2026-09-09 20:51:33 -0400

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s2) > len(s2):
+            return False
+
+        charSet1 = [0]*26
+        charSet2 = [0]*26
+
+        for i in range(len(s1)):
+            charSet1[ord(s1[i])-ord('a')] +=1
+            charSet2[ord(s2[i])-ord('a')] +=1
+
+        matches = 0
+
+        for i in range(26):
+            matches+= (1 if charSet1[i]==charSet2[i] else 0) 
+
+        l =0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+
+            index = ord(s2[r]) - ord('a')
+            charSet2[index] +=1
+            if charSet2[index] == charSet1[index]:
+                matches+=1
+            elif charSet1[index] == charSet2[index] - 1:
+                matches-=1
+
+            index = ord(s2[l]) - ord('a')
+            charSet2[index] -=1
+            if charSet2[index] == charSet1[index]:
+                matches +=1
+            elif charSet1[index] == charSet2[index] + 1:
+                matches-=1 
+
+            l+=1
+
+        return matches == 26
